8ad8014c85da6142f58f97edf9de77ac8634be9b
taisuke-ohmi <taisukeohmi0420@gmail.com>
2018-08-18 17:24:15 +0900

    パスワードなしでプロフィール編集できるように

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root 'pages#home'
-  devise_for :users
+  devise_for :users,
+    controllers: { registrations: 'registrations' }
   resources :users, only: %i(show)
 end
