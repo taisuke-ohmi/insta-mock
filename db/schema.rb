8e474f2e36905c64fce92a156273f50a59efe998
taisuke-ohmi <taisukeohmi0420@gmail.com>
2018-08-18 17:32:41 +0900

    Postモデル作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180818081519) do
+ActiveRecord::Schema.define(version: 20180818083017) do
+
+  create_table "posts", force: :cascade do |t|
+    t.string "caption"
+    t.integer "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_posts_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
