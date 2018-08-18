ae6566d6275daac2194df7d528a14628717e50de
taisuke-ohmi <taisukeohmi0420@gmail.com>
2018-08-18 17:16:00 +0900

    website, bioカラム追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180818080504) do
+ActiveRecord::Schema.define(version: 20180818081519) do
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -22,6 +22,8 @@ ActiveRecord::Schema.define(version: 20180818080504) do
     t.datetime "updated_at", null: false
     t.string "name", default: "", null: false
     t.string "image"
+    t.string "website"
+    t.text "bio"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
