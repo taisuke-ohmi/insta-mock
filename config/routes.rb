0e3f074f0e8f85d02416329521a5fffaa91aad16
taisuke-ohmi <taisukeohmi0420@gmail.com>
2018-08-21 02:06:48 +0900

    一覧画面をpartial化

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root 'pages#home'
+  root 'posts#index'
   devise_for :users,
     controllers: { registrations: 'registrations' }
   resources :users, only: %i(show)
