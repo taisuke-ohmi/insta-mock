a6e7555a5070c4c3422e3b9ca4c31a39c058c7f3
taisuke-ohmi <taisukeohmi0420@gmail.com>
2018-08-21 01:04:07 +0900

    routing変更

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,7 @@ Rails.application.routes.draw do
   devise_for :users,
     controllers: { registrations: 'registrations' }
   resources :users, only: %i(show)
+  resources :posts, only: %i(index new create show) do
+    resources :photos, only: %i(create)
+  end
 end
